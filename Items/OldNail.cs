f135955b87eb85a8b24c6ed261de888d96393bfa
Alphi999 <cookayterraria@gmail.com>
2018-08-11 17:06:54 +0400

    A good few changes
    All of em' by sen, thank him

--- Items/OldNail.cs
+++ Items/OldNail.cs
@@ -1,9 +1,9 @@
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria;
 
 namespace HollowKnightMod.Items
 {
-    // ReSharper disable once UnusedMember.Global
     public class OldNail : ModItem
     {
         public override void SetStaticDefaults()
@@ -19,21 +19,27 @@ namespace HollowKnightMod.Items
             item.width = 40;
             item.height = 40;
             item.useTime = 20;
-            item.useAnimation = 20;
-            item.useStyle = 1;
             item.knockBack = 6;
             item.value = 10000;
             item.rare = 2;
             item.UseSound = SoundID.Item1;
+            item.shoot = mod.ProjectileType("OldNail");
+            item.useAnimation = 25;
+            item.useStyle = 5;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.autoReuse = false;
+            item.noMelee = true;
+            item.shootSpeed = 0f;
         }
 
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.DirtBlock, 10);
-            recipe.AddTile(TileID.WorkBenches);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
+        /*public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);               we should probably change this -s
+			recipe.AddTile(TileID.WorkBenches);               ok yeah fair enough honestly it spawns with the player -a
+			recipe.SetResult(item);
+			recipe.AddRecipe();
+		}*/
     }
 }
