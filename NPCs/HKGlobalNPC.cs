1f4a4b211b10915295962f9818ae54953ef79a72
Sentri <sentricast@gmail.com>
2018-08-13 21:27:50 +0300

    POON

--- NPCs/HKGlobalNPC.cs
+++ NPCs/HKGlobalNPC.cs
@@ -20,7 +20,33 @@ namespace HollowKnightMod.NPCs
                 Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("PaleOre"));
             }
 
-            if (npc.type == )
+            if ((npc.type == NPCID.SkeletronPrime || 
+                (npc.type == NPCID.Retinazer && !NPC.AnyNPCs(NPCID.Spazmatism)) ||
+                (npc.type == NPCID.Spazmatism && !NPC.AnyNPCs(NPCID.Retinazer)) ||
+                (npc.type == NPCID.TheDestroyer) && !NPC.downedMechBossAny))
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("PaleOre"));
+            }
+
+            if (npc.type == NPCID.Plantera && !NPC.downedPlantBoss)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("PaleOre"));
+            }
+
+            if (npc.type == NPCID.Golem && !NPC.downedGolemBoss)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("PaleOre"));
+            }
+
+            if (npc.type == NPCID.DukeFishron && !NPC.downedFishron)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("PaleOre"));
+            }
+
+            if (npc.type == NPCID.CultistBoss && !NPC.downedAncientCultist)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("PaleOre"));
+            }
 
             return true;
         }
